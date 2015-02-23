01e5651809db182977e979c413cd7cb92942fedc
Jan Stepanek <jan.stepanek@nubium.cz>
2015-02-23 19:01:32 +0100

    commands/recreate_branch : recreate_branch looks only for direct merges
    It ignores merge commits in other branches than in the recreated one.

--- lib/git_bpf/commands/recreate-branch.rb
+++ lib/git_bpf/commands/recreate-branch.rb
@@ -191,7 +191,9 @@ class RecreateBranch < GitFlow/'recreate-branch'
 
     merges.split("\n").each do |commits|
       parents = commits.split("\s")
-      commit = parents.shift
+
+      commit = git('name-rev', parents.shift, '--name-only').strip
+      next unless commit.include? source
 
       parents.each do |parent|
         name = git('name-rev', parent, '--name-only', "--refs=#{remote_recreate}").strip
