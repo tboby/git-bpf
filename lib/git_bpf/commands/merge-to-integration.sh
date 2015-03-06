327408092300180b3bb0f272328e752fa2b88701
Uri Hendler <uri@goseamless.co.za>
2015-03-06 15:49:39 +0200

    Check if merge resulted in conflicts and output relevant error message

--- lib/git_bpf/commands/merge-to-integration.sh
+++ lib/git_bpf/commands/merge-to-integration.sh
@@ -21,31 +21,41 @@ if [ "$currBranch" != "integration" ]; then
 		echo.
 		echo "Checking out integration branch..."
 		git checkout integration
-		echo.
-		echo "Syncing integration branch from remote..."
-		git pull origin integration
 	else
 		exit 1
 	fi
-else
-	echo.
-	echo "Syncing integration branch from remote..."
-	git pull origin integration
 fi
+
+remote=$(git config --get gitbpf.remotename)
+
+echo.
+echo "Syncing integration branch from remote '$remote'..."
+syncIntegration=$(git pull $remote integration)
+echo "$syncIntegration"
 
 echo.
 echo -n "Merging feature branch "
 echo -e -n "\033[1;36m$1"
 echo -e "\033[0m into integration branch..."
 
+# Run git merge with no fast-forward option
 mergeResult=$(git merge --no-ff $1)
 echo.
 if [ "$mergeResult" = "Already up-to-date." ]; then
 	echo -e "\033[1;32mFeature branch has already been merged into integration branch."
-	echo "Nothing to do."
-else
-	echo -e "\033[1;32mMerging complete."
 	echo.
-	echo -e -n "\033[1;31mPlease resolve any conflicts now!"
+	echo "If you are working on a shared feature branch, another developer may have already merged your commits."
+else
+	# Check if the merge command resulted in conflicts
+	numConflicts="$(echo $mergeResult | grep "Automatic merge failed; fix conflicts and then commit the result." | wc -l | awk {'print $1'})"
+	if [[ ( "$numConflicts" > 0 ) ]]; then
+		echo -e "\033[1;32mMerging complete."
+		echo.
+		echo -e -n "\033[1;31mConflicts were detected. Please resolve any conflicts now and commit your changes."
+	else
+		echo -e "\033[1;32mMerging complete."
+		echo.
+		echo -e -n "\033[1;32mNo conflicts detected."
+	fi
 fi
 echo -e '\033[0m'
