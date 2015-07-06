a97274e509d7fb1f468f5f168aa01c31347e9cfb
Uri Hendler <uri@goseamless.co.za>
2015-07-06 10:57:09 +0200

    Fixed merge-to-integration script to behave correctly on non-Windows platforms

--- lib/git_bpf/commands/merge-to-integration.sh
+++ lib/git_bpf/commands/merge-to-integration.sh
@@ -16,11 +16,11 @@ fi
 if [ "$currBranch" != "integration" ]; then
 	currBranchNotIntegration=true
 	echo -e "\033[1;36mYou are currently on branch $currBranch."
-	echo -e -n "\033[1;33mCheckout integration branch now? "
-	echo -e -n '\033[0m'
+	echo -en "\033[1;33mCheckout integration branch now? "
+	echo -en '\033[0m'
 	read choice
 	if [ "$choice" = "y" ]; then
-		echo.
+		echo
 		echo "Checking out integration branch..."
 		git checkout integration
 	else
@@ -30,22 +30,22 @@ fi
 
 remote=$(git config --get gitbpf.remotename)
 
-echo.
+echo
 echo "Syncing integration branch from remote '$remote'..."
 syncIntegration=$(git pull $remote integration)
 echo "$syncIntegration"
 
-echo.
+echo
 echo -n "Merging feature branch "
-echo -e -n "\033[1;36m$1"
+echo -en "\033[1;36m$1"
 echo -e "\033[0m into integration branch..."
 
 # Run git merge with no fast-forward option
 mergeResult=$(git merge --no-ff $1)
-echo.
+echo
 if [ "$mergeResult" = "Already up-to-date." ]; then
 	echo -e "\033[1;32mFeature branch has already been merged into integration branch."
-	echo.
+	echo
 	echo "If you are working on a shared feature branch, another developer may have already merged your commits."
 	echo -e '\033[0m'
 	exit
@@ -53,31 +53,31 @@ fi
 # Check if the merge command resulted in conflicts
 numConflicts="$(echo $mergeResult | grep "Automatic merge failed; fix conflicts and then commit the result." | wc -l | awk {'print $1'})"
 echo -e "\033[1;32mMerging complete."
-echo.
+echo
 
 if [ $numConflicts -gt 0 ]; then
-	echo -e -n "\033[1;31mConflicts were detected. Please resolve any conflicts now and commit your changes."
+	echo -en "\033[1;31mConflicts were detected. Please resolve any conflicts now and commit your changes."
 	echo -e '\033[0m'
 	exit
 fi
 
 echo -e "\033[1;32mNo conflicts detected."
-echo.
+echo
 
 # Since no conflicts were detected, offer to push the integration branch to the remote and checkout the original branch
-echo -e -n "\033[1;33mPush integration branch to '$remote' now? "
-echo -e -n '\033[0m'
+echo -en "\033[1;33mPush integration branch to '$remote' now? "
+echo -en '\033[0m'
 read choice
 if [ "$choice" = "y" ]; then
 	pushResult=$(git push $remote integration)
 	echo $pushResult
 	if ($pushResult); then
 		if [ "$currBranchNotIntegration" = true ]; then
-			echo -e -n "\033[1;33mCheckout your previous branch ($currBranch) now? "
-			echo -e -n '\033[0m'
+			echo -en "\033[1;33mCheckout your previous branch ($currBranch) now? "
+			echo -en '\033[0m'
 			read choice
 			if [ "$choice" = "y" ]; then
-				echo.
+				echo
 				echo "Checking out $currBranch branch..."
 				git checkout $currBranch
 			fi
