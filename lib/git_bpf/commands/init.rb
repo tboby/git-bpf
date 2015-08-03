923972a1a3c0552a62eebe8208f053991783f077
Uri Hendler <uri@goseamless.co.za>
2015-08-03 13:13:14 +0200

    Removed pre-commit hook from init

--- lib/git_bpf/commands/init.rb
+++ lib/git_bpf/commands/init.rb
@@ -194,7 +194,6 @@ class Init < GitFlow/'init'
     #
     hooks_dir = File.join(target.git_dir, "hooks")
     hooks = [
-      'pre-commit',
       'post-commit',
       'post-checkout',
       'pre-push'
